7698a678406d8c4e3721f25bacf0796e252f6b84
andriandwiii <andriands025@gmail.com>
2026-06-19 06:41:03 +0700

    fix: revert to original 3 Poppins fonts (Regular, SemiBold, Bold)

--- Frontend/app/_layout.tsx
+++ Frontend/app/_layout.tsx
@@ -45,7 +45,6 @@ const RootLayout = () => {
 
   const [fontsLoaded, fontError] = useFonts({
     'Poppins-Regular': require('../assets/fonts/Poppins-Regular.ttf'),
-    'Poppins-Medium': require('../assets/fonts/Poppins-Medium.ttf'),
     'Poppins-SemiBold': require('../assets/fonts/Poppins-SemiBold.ttf'),
     'Poppins-Bold': require('../assets/fonts/Poppins-Bold.ttf'),
   });
